eb506746d0795753a56e940ca9b74cbbee003199
Erica Pastor Gracia <117344247+epastor-embou@users.noreply.github.com>
2023-03-30 09:51:48 +0200

    Sample Games + redirect to games.php View

--- app/Controllers/Games.php
+++ app/Controllers/Games.php
@@ -4,5 +4,29 @@ namespace App\Controllers;
 
 class Games extends BaseController
 {
-
+    function list(): string
+    {
+        $data['gamesSample'] = [
+            [
+                'userId' => 3,
+                'username' => 'effyelle',
+                'title' => 'Timeless',
+                'img_src' => '18915186841312867612.jpg'
+            ],
+            [
+                'userId' => 1,
+                'username' => 'keinarman',
+                'title' => 'Indamar Chronicles',
+                'img_src' => '18915186841312867612.jpg'
+            ],
+            [
+                'userId' => 2,
+                'username' => 'fedora',
+                'title' => 'Velkia Tales - Omen Seekers',
+                'img_src' => '18915186841312867612.jpg'
+            ]
+        ];
+        // Database connect, get games, then load
+        return template('games', $data);
+    }
 }

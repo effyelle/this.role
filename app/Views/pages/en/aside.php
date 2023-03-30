40e1eb69ca0ef65a681e8f3b30581b9965f46d7b
Erica Pastor Gracia <117344247+epastor-embou@users.noreply.github.com>
2023-03-30 12:44:53 +0200

    Comment suggestions mail

--- app/Views/pages/en/aside.php
+++ app/Views/pages/en/aside.php
@@ -122,24 +122,23 @@
         </div>
         <!--end::Aside menu-->
         <!--begin::Footer-->
+        <!--
         <div class="aside-footer flex-column-auto pt-5 pb-7 px-5" id="kt_aside_footer">
-            <!-- LINK MODAL modal_suggestions -->
             <a data-bs-toggle="modal" data-bs-target="#modal_suggestions"
                class="btn btn-bg-light btn-color-gray-500 btn-active-color-gray-900 w-100">
                 <span class="btn-label">Buzón de sugerencias</span>
-                <!--begin::Svg Icon | path: icons/duotune/general/gen005.svg-->
                 <span class="svg-icon btn-icon svg-icon-2">
-				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M6 8.725C6 8.125 6.4 7.725 7 7.725H14L18 11.725V12.925L22 9.725L12.6 2.225C12.2 1.925 11.7 1.925 11.4 2.225L2 9.725L6 12.925V8.725Z"
-                          fill="currentColor"></path>
-					<path opacity="0.3"
-                          d="M22 9.72498V20.725C22 21.325 21.6 21.725 21 21.725H3C2.4 21.725 2 21.325 2 20.725V9.72498L11.4 17.225C11.8 17.525 12.3 17.525 12.6 17.225L22 9.72498ZM15 11.725H18L14 7.72498V10.725C14 11.325 14.4 11.725 15 11.725Z"
-                          fill="currentColor"></path>
-				</svg>
-			</span>
-                <!--end::Svg Icon-->
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6 8.725C6 8.125 6.4 7.725 7 7.725H14L18 11.725V12.925L22 9.725L12.6 2.225C12.2 1.925 11.7 1.925 11.4 2.225L2 9.725L6 12.925V8.725Z"
+                              fill="currentColor"></path>
+                        <path opacity="0.3"
+                              d="M22 9.72498V20.725C22 21.325 21.6 21.725 21 21.725H3C2.4 21.725 2 21.325 2 20.725V9.72498L11.4 17.225C11.8 17.525 12.3 17.525 12.6 17.225L22 9.72498ZM15 11.725H18L14 7.72498V10.725C14 11.325 14.4 11.725 15 11.725Z"
+                              fill="currentColor"></path>
+                    </svg>
+			    </span>
             </a>
         </div>
+        -->
         <!--end::Footer-->
         <div class="modal fade show" tabindex="-1" id="modal_suggestions" aria-modal="true" role="dialog">
             <div class="modal-dialog">

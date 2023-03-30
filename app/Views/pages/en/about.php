3a452eb0e4f2a729c00edc39265be6d35f431385
Erica Pastor Gracia <117344247+epastor-embou@users.noreply.github.com>
2023-03-30 13:56:46 +0200

    Create about.php

---
+++ app/Views/pages/en/about.php
@@ -0,0 +1,23 @@
+<!--begin::Row-->
+<div class="row g-5 g-lg-10">
+    <!--begin::Col-->
+    <div class="mb-xl-10">
+        <!--begin::List Widget 6-->
+        <div class="card h-xl-100">
+            <!--begin::Header-->
+            <div class="card-header">
+                <div class="card-toolbar gap-5">
+                    <h2><?= $title ?></h2>
+                </div>
+            </div>
+            <!--end::Header-->
+            <!--begin::Body-->
+            <div class="card-body">
+            </div>
+            <!--end::Body-->
+        </div>
+        <!--end::List Widget 6-->
+    </div>
+    <!--end::Col-->
+</div>
+<!--end::Row-->

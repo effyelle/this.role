9831debfb856fe00558262cc041ecfa245ebd3f6
Erica Pastor Gracia <117344247+epastor-embou@users.noreply.github.com>
2023-03-30 12:43:12 +0200

    Link sections sample

--- app/Controllers/Compendium.php
+++ app/Controllers/Compendium.php
@@ -4,8 +4,20 @@ namespace App\Controllers;
 
 class Compendium extends BaseController
 {
-    function index() {
-        $data['mainPages']=[];
-        return template('', $data);
+    function index()
+    {
+        $compendiumSectionsFolder = '/assets/appmedia/compendium/sections_profile/';
+        if (!is_dir(__DIR__ . '/../../public' . $compendiumSectionsFolder)) $compendiumSectionsFolder = 'not_a_folder/';
+        $data['mainSections'] = [
+            ['name' => 'Classes', 'img_src' => $compendiumSectionsFolder . 'classes.png'],
+            ['name' => 'Races', 'img_src' => $compendiumSectionsFolder . 'races.png'],
+            ['name' => 'Backgrounds', 'img_src' => $compendiumSectionsFolder . 'backgrounds.png'],
+            ['name' => 'Spells', 'img_src' => $compendiumSectionsFolder . 'spells.png'],
+            ['name' => 'Items', 'img_src' => $compendiumSectionsFolder . 'armor.png'],
+            ['name' => 'Bestiary', 'img_src' => $compendiumSectionsFolder . 'bestiary.png'],
+            ['name' => 'Conditions & Diseases', 'img_src' => $compendiumSectionsFolder . 'conds_diseases.png'],
+            ['name' => 'Actions', 'img_src' => $compendiumSectionsFolder . 'actions.png'],
+        ];
+        return template('compendium', $data);
     }
 }

cae8c0584b758a5862c61ed1430f814cbfba1544
Erica Pastor Gracia <117344247+epastor-embou@users.noreply.github.com>
2023-04-11 09:50:02 +0200

    Tokens for account confirmation

---
+++ app/Models/TokenModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+use \Config\Database as db;
+
+class TokenModel extends Model
+{
+    protected $table = 'tokens';
+    protected $primaryKey = 'token_id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ['token_id', 'token', 'token_user', 'token_expires', 'token_deleted'];
+
+    function get(string $token): array|bool
+    {
+        $builder = db::connect()->table($this->table);
+        $builder->select('token, token_user');
+        $builder->where('token', $token);
+        $builder->where('token_expires <', date('Y-m-d h:i:s', time()));
+        if ($token = $builder->get()->getResultArray()) {
+            if (count($token) === 1) return $token[0];
+        }
+        return false;
+    }
+
+    function new(string $token, int $user): array|bool
+    {
+        $builder = db::connect()->table($this->table);
+        if ($builder->insert(['token' => $token, 'token_user' => $user])) {
+            return $this->get($token);
+        }
+        return false;
+    }
+}

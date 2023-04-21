ff11b8cb7697f5d57ee46746de09a4d8fbce8d27
Erica Pastor Gracia <117344247+epastor-embou@users.noreply.github.com>
2023-04-21 10:00:05 +0200

    Format emails format for mail conf and pwd recovery

--- app/Views/templates/mail/confirm_account_html.php
+++ app/Views/templates/mail/confirm_account_html.php
@@ -1,26 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Email</title>
     <meta name="Title" content="">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="Author" content="wfl">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <meta name="Copyright" content="Copyright by SALESmanago">
     <meta name="robots" content="noindex">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="x-apple-disable-message-reformatting">
+    <title>Email</title>
+    <link rel="icon" href="https://i.pinimg.com/originals/59/9f/ce/599fce92895fdefbc4fcf610ddef1ff4.png" sizes="64x64"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>
+    <style>
+        a, a:visited, a:active {
+            color: #ffb1a0;
+        }
+
+        a:hover {
+            color: #ffc3ba;
+        }
+    </style>
 </head>
-<body style="text-align: center;font-size: 28px;font-weight: 600;color: #eddec6; background: #000000;">
-<div style="margin:auto;padding:150px 40px;height: 200px; width: 600px;
-    background: url('https://i.pinimg.com/564x/87/18/65/871865109e0425b869feee16a384402b.jpg') no-repeat;background-size: cover;">
-    <p style="margin: 20px auto;">
-        Welcome to This.Role! To confirm your account, please follow this
-        <a href="this.role.com/account/confirm/<?= $token ?>" style="text-decoration: underline;color: #f0f0f0;">
-            link
-        </a>.
-    </p>
-    <p style="margin: 20px auto;">
-        If you didn't initialize this process, you can ignore this email.
+<body style="text-align:center;font-family:Poppins, Helvetica, 'sans-serif';font-size:16px;font-weight:500;
+    color:#ffffff;background:#425983;padding:120px 0;">
+<div style="margin:auto;width:200px;height:200px;position:relative;">
+    <img alt="This.Role" width="280" height="280" style="opacity: 0.5;position:absolute;top:-40px;left:-40px;"
+         src="https://i.pinimg.com/originals/e8/83/e1/e883e15044caca9e0dc4fe6073deabdc.png"/>
+    <div style="margin:auto;text-align:center;width:200px;height:200px;display:flex;justify-content:center;
+    align-items:center;flex-direction:column;position:absolute;top:0;left:0;">
+        <p style="margin:0 auto;">The Lunar Dragon welcomes you to <b style="font-size:17px;">This.Role!</b></p>
+        <p style="margin:0 auto;">
+            To confirm your account, please follow
+            <a href="http://this.role.com/account/confirm/<?= $token ?>"
+               style="transition:all 0.3s ease;{color:#1e7769;}:visited:active{color:#1e7769;}:hover{color:#2b9685;};">
+                <b>this link</b>
+            </a>
+        </p>
+    </div>
+    <p style="margin:0 auto;font-size:14px;position:absolute;top:250px;font-weight:400">
+        <i>If you didn't initialize this process, you can ignore this email</i>
     </p>
 </div>
 </body>

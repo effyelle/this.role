1a82eeadc24c30a030b02beb5e343dd30294ac2b
Erica Pastor Gracia <117344247+epastor-embou@users.noreply.github.com>
2023-04-25 11:24:55 +0200

    Move scripts to other file

--- app/Views/pages/games/game.php
+++ app/Views/pages/games/game.php
@@ -1,4 +1,7 @@
-<div class="this-game w-100 h-100 bg-white"></div>
+<div class="this-game w-100 h-100 bg-white">
+    <?php var_dump($game ?? '');
+    ?>
+</div>
 <!--begin::Modal-->
 <div class="modal fade" tabindex="-1" id="modal_journal">
     <div class="modal-dialog">
@@ -59,118 +62,16 @@
 <!--end::Modal-->
 <script src="/assets/plugins/custom/ckeditor/ckeditor-classic.bundle.js"></script>
 <script src="/assets/js/custom/apps/ckeditor/CKEditor.js"></script>
-<script type="text/javascript" src="/assets/js/custom/games/formatGame.js"></script>
+<script type="text/javascript" src="/assets/js/custom/games/game.js"></script>
 <script type="text/javascript" src="/assets/js/custom/games/Board.js"></script>
-<script type="text/javascript" src="/assets/js/custom/games/Journal.js"></script>
 <script>
+    // * Game details from DATABASE * //
+    const dbGame =<?php echo json_encode($game ?? []) ?>;
+    const session =<?php echo json_encode($_SESSION); ?>;
+
     document.addEventListener('DOMContentLoaded', function () {
-
-        // * Board intance * //
-        const board = new Board('.btn.dice');
-
-        // * Chat object * //
-        const chat = new Chat('.chat-messages');
-
-        // *********************************** //
-        // * Listen to dices buttons pressed * //
-        // *********************************** //
-        $('.btn.dice').click(function () {
-            chat.formatMessage({
-                src: "",
-                msg: board.dices[this.value].roll(),
-                sender: $('#charsheet_selected').find(':selected').text(),
-                msgType: "rollDice",
-                dice: this.value,
-                rolling: $('#roll-' + this.value).val()
-            });
-        });
-
-        // * Chat textarea constant * //
-        const chatText = document.querySelector('.chat-bubble textarea');
-        // * Chat textarea holder * //
-        let chatMessage = '';
-
-        // ******************************* //
-        // * Listen to chat pressed keys * //
-        // ******************************* //
-        chatText.addEventListener('keypress', function (e) {
-            // Save key if not Enter
-            if (e.key !== 'Enter') {
-                chatMessage += e.key;
-            }
-
-            if (e.key === 'Enter') { // If Enter key
-                if (e.shiftKey) { // If Shift+Enter
-                    chatMessage += '<br/>'; // Add line break
-                    return; // Return
-                }
-                // If Enter without Shift
-                e.preventDefault(); // Prevent textarea line break
-                addChatMessage(chatMessage.trim());
-            }
-        });
-
-        // ********************************* //
-        // * Listen to send button in chat * //
-        // ********************************* //
-        document.querySelector('.chat-bubble ~ div .btn').addEventListener('click', function () {
-            addChatMessage(chatMessage.trim());
-        });
-
-        /**
-         * Add Chat Message
-         * @param text
-         */
-        function addChatMessage(text) {
-            // You do not check chatMessage variable here for it might have <br/> but nothing else
-            if (text !== '') { // Check textarea VALUE is not empty
-                chat.formatMessage({ // Submit message
-                    sender: $('#charsheet_selected').find(':selected').text(),
-                    src: "",
-                    msg: chatMessage.trim(),
-                    msgType: "chatMessage"
-                });
-                this.value = ""; // Empty chat textarea
-                chatMessage = ''; // Empty holder variable
-            }
+        if (dbGame !== [] && dbGame.game_id) {
+            initBoard(dbGame, session);
         }
-
-        // * Journal intance * //
-        const journal = new Journal('journal');
-
-        // ********************************************* //
-        // * Add journal item when save button clicked * //
-        // ********************************************* //
-        $('#modal_journal .save_btn').click(function () {
-            let journalItemTitle = $('#journal_title-input');
-            let journalItemType = $('#journal-item_type').find(':selected').val();
-            // Boolean to check if one or more fields are empty
-            let fieldsRequired = journalItemTitle.val() !== '' && journalItemType !== "-1";
-            // Toggle spinner in save button
-            toggleProgressSpinner();
-            // Show error message if one or more fields are empty
-            $('#modal_journal .error').toggleClass('d-none', fieldsRequired);
-            // If no fields are empty
-            if (fieldsRequired) {
-                // Add item to HTML
-                journal.formatJournalItem(journalItemType, {
-                    src: '',
-                    title: journalItemTitle.val(),
-                });
-                // Dismiss journal modal
-                $('#modal_journal .dismiss_btn').click();
-            }
-            // Always stop progress spinner at the end of all actions
-            toggleProgressSpinner(false);
-        });
-
-        // ********************************** //
-        // * Empty journal modal on closure * //
-        // ********************************** //
-        $('#modal_journal').on('hidden.bs.modal', function () {
-            $('#journal_title-input').val('');
-            $('#journal-item_type option[value=-1]').prop('selected', true);
-            $('#modal_journal input').prop('checked', false);
-        });
     });
 </script>

754770f241fe6a80123f786127f092d7504f09cd
Erica Pastor Gracia <117344247+epastor-embou@users.noreply.github.com>
2023-05-27 15:31:09 +0200

    Update App.php

--- thisRoleApp/app/Config/App.php
+++ thisRoleApp/app/Config/App.php
@@ -18,7 +18,7 @@ class App extends BaseConfig
      *    http://example.com/
      */
     public string $baseURL = 'http://localhost'; // Localhost
-    // public string $baseURL='ec2-52-54-134-78.compute-1.amazonaws.com/index.php'; // AWS
+    // public string $baseURL='http://ec2-52-54-134-78.compute-1.amazonaws.com/index.php'; // AWS
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.

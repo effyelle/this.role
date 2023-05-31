40348f4444486255322bc481730d9ca0156aa2b7
Erica Pastor Gracia <117344247+epastor-embou@users.noreply.github.com>
2023-05-31 15:29:23 +0200

    Update Mailer url

--- thisRoleApp/app/Controllers/Mailer.php
+++ thisRoleApp/app/Controllers/Mailer.php
@@ -27,7 +27,7 @@ class Mailer
     /**
      * @var string
      */
-    protected string $fromEmail = 'this.role@outlook.com';
+    protected string $fromEmail = 'this.role@outlook.es';
 
     /**
      * @var string
@@ -37,7 +37,7 @@ class Mailer
     /**
      * @var string
      */
-    protected string $pwd = 'Aqws!123';
+    protected string $pwd = '@_Druid7';
 
     /**
      * @var string
